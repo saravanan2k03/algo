4965cb5144161bf0dd117d8f4e529904aa7dac73
saravanan2k03 <sarothehacker@gmail.com>
2024-01-21 08:20:03 +0530

    code written in Majority_element

--- Majority_Element.py
+++ Majority_Element.py
@@ -2,6 +2,11 @@ nums = [3,2,3]
 res={}
 
 for i in nums:
-    res[i] =nums.count(i)
+    if i in res:
+        res[i] +=1
+    else:
+        res[i] =1
 print(res)
 
+op=max(res,key=res.get)
+print(op)
