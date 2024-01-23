45abb2c968c3c05f462acdbd94644c310a5f2e2b
saravanan2k03 <sarothehacker@gmail.com>
2024-01-23 11:02:12 +0530

    solved container with water

---
+++ Container_With_Water.py
@@ -0,0 +1,21 @@
+# height = [1,8,6,2,5,4,8,3,7]
+height = [1,1]
+
+
+left = 0
+right = len(height)-1
+maxArea = 0
+
+
+while left < right:
+    area = min(height[left],height[right]) * abs(left - right)
+    maxArea = max(maxArea,area)
+
+    if height[left] < height[right]:
+        left +=1
+    else :
+        right -=1
+
+
+
+print(maxArea)
