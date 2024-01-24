55690662f35243e909cbd65832295590e3af5556
saravanan2k03 <sarothehacker@gmail.com>
2024-01-24 07:07:35 +0530

    comment the Binary_search

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,8 +1,11 @@
 nums = [-1,0,3,5,9,12] 
 target = 9
 
-
-
+nums.sort()
+# mid = len(nums) / 2
+# print(round(mid))
+# mid = int(round(mid))
+# print(nums[mid])
 if target in nums:
     for index,value in enumerate(nums):
         print(index,value)
@@ -10,3 +13,6 @@ if target in nums:
             print("target reached", index)
 else:
     print("target not found", target)
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,10 @@
-        # print(triplet)
+nums = [2,7,11,15]
+target = 9
+
+for i in range(len(nums)):
+    for j in range(len(nums)):
+        if i != j:
+            sumval = nums[i] + nums[j]
+            if sumval == target:
+               print(i+1,j+1)
+               print(sumval)
